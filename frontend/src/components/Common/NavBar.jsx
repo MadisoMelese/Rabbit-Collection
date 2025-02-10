b4e1fbae48dbf2e389b2dbeac426f5ef9a75aa5f
Madiso Melese <madishamadiso00@gmail.com>
2025-02-10 23:55:55 +0300

    search bar added in fronten

---
+++ frontend/src/components/Common/NavBar.jsx
@@ -0,0 +1,71 @@
+import { Link } from "react-router-dom";
+import {
+  HiOutlineUser,
+  HiBars3BottomRight,
+  HiOutlineShoppingBag,
+} from "react-icons/hi2";
+import SearchBar from "./SearchBar";
+const NavBar = () => {
+  return (
+    <>
+      <nav className="container mx-auto flex items-center justify-between py-4 px-6">
+        {/* Left- Logo */}
+        <div>
+          <Link to="/" className="text-2xl font-medium">
+            Rabbit
+          </Link>
+        </div>
+
+        {/* center -Naviagtion Links*/}
+        <div className="hidden md:flex space-x-6">
+          <Link
+            to={"#"}
+            className="text-gray-700 hover:text-black text-sm font-medium uppercase"
+          >
+            Men
+          </Link>
+          <Link
+            to={"#"}
+            className="text-gray-700 hover:text-black text-sm font-medium uppercase"
+          >
+            Women
+          </Link>
+          <Link
+            to={"#"}
+            className="text-gray-700 hover:text-black text-sm font-medium uppercase"
+          >
+            Top Wear
+          </Link>
+          <Link
+            to={"#"}
+            className="text-gray-700 hover:text-black text-sm font-medium uppercase"
+          >
+            bottom wear
+          </Link>
+        </div>
+
+        {/* Right-Naviagtion Links */}
+
+        <div className="flex items-center space-x-4">
+          <Link className="hover:text-black">
+            <HiOutlineUser className="size-6 text-gray-700" />
+          </Link>
+
+          <button className="relative hover:text-black">
+            <HiOutlineShoppingBag className="size-6 text-gray-700"  />
+            <span className="absolute -top-1 bg-[#ea2e0e] text-white text-xs rounded-full px-2 py-0.5">5</span>
+          </button>
+
+          {/* Search Icon */}
+          <SearchBar />
+
+          <button className="md:hidden">
+            <HiBars3BottomRight className="size-6 text-gray-700" />
+          </button>
+        </div>
+      </nav>
+    </>
+  );
+};
+
+export default NavBar;
---
+++ frontend/src/components/Layout/TopBar.jsx
@@ -0,0 +1,41 @@
+import { TbBrandMeta } from "react-icons/tb";
+import { IoLogoInstagram, IoLogoLinkedin, IoLogoTwitter } from "react-icons/io";
+const TopBar = () => {
+  return (
+    <div className="bg-[#ea2e0e] text-white">
+      <div className="container mx-auto flex items-center sm:justify-between justify-center py-3 px-5">
+        {/* Social Media Icons */}
+        <div className="hidden items-center space-x-4 md:flex">
+          <a href="#" className="hover:text-gray-300">
+            <TbBrandMeta className="size-5" />
+          </a>
+          <a href="#" className="hover:text-gray-300">
+            <IoLogoInstagram className="size-5" />
+          </a>
+          <a href="#" className="hover:text-gray-300">
+            <IoLogoLinkedin className="size-5" />
+          </a>
+          <a href="#" className="hover:text-gray-300">
+            <IoLogoTwitter className="size-5" />
+          </a>
+        </div>
+
+        {/* Motivation  */}
+        <div className="sm:text-sm text-lg">
+          <span className="">
+            We ship world-wide - Fast and reliable shipping!
+          </span>
+        </div>
+
+        {/* Contact */}
+        <div className="text-sm hidden sm:flex">
+          <a href="tel:+251954233154" className="hover:text-gray-300">
+            (+251) 954233154
+          </a>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default TopBar;
