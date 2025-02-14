50a0dc969530d76b16917555c46fd582ed883666
Madiso Melese <madishamadiso00@gmail.com>
2025-02-14 23:54:48 +0300

    login and sign up pages added

---
+++ frontend/src/pages/Login.jsx
@@ -0,0 +1,56 @@
+import { useState } from "react";
+import { Link } from "react-router-dom";
+import login from '../assets/login.webp'
+const Login = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  return (
+    <div className="flex">
+      <div className="w-full flex md:w-1/2 flex-col justify-center items-center p-8 md:p-12">
+        <form className="w-full max-w-md bg-white p-8 rounded-lg border shadow-sm">
+          <div className="flex justify-center mb-6">
+            <h2 className="text-xl font-medium">Rabbit</h2>
+          </div>
+          <h2 className="text-2xl font-bold text-center mb-6">Hey there! 👋</h2>
+          <p className="text-center mb-6 ">
+            Enter your username and password to Login.
+          </p>
+          <div className="mb-4">
+            <label className="block text-sm font-semibold mb-2">Email</label>
+            <input
+              type="email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              placeholder="Enter your email Address"
+              className="w-full p-2 border  rounded invalid:border-pink-500 invalid:text-pink-600 focus:border-sky-500 focus:outline focus:outline-sky-500 focus:invalid:border-pink-500 focus:invalid:outline-pink-500 "
+              autoFocus
+            />
+          </div>
+          <div className="mb-4">
+            <label className="block text-sm font-semibold mb-2">Password</label>
+            <input
+              type="password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              placeholder="Enter your password"
+              className="w-full p-2 border  rounded invalid:border-pink-500 invalid:text-pink-600 focus:border-sky-500 focus:outline focus:outline-sky-500 focus:invalid:border-pink-500 focus:invalid:outline-pink-500"
+            />
+          </div>
+          <button className="w-full bg-black text-white p-2 rounded-lg font-semibold hover:bg-gray-800 transition">sign In</button>
+          <p className="mt-6 text-center text-sm">Don&apos;t have an account? <Link to='/register' className="text-blue-500 hover:underline">Sign Up</Link></p>
+          
+        </form>
+      </div>
+
+      <div className="hidden md:block w-1/2 bg-gray-800">
+      <div className="w-full flex flex-col justify-center items-center">
+        <img src={login} alt="Login image"
+        className="h-[750px] w-full object-cover" 
+        />
+      </div>
+      </div>
+    </div>
+  );
+};
+
+export default Login;
---
+++ frontend/src/pages/SignUp.jsx
@@ -0,0 +1,88 @@
+import { useState } from "react";
+import { Link } from "react-router-dom";
+import register from "../assets/register.webp";
+
+const SignUp = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [fullname, setFullname] = useState("");
+
+  const handleSubmit = (e)=>{
+    e.preventDefault()
+    console.log("user info: ", {fullname, email, password})
+  }
+  
+  return (
+    <div className="flex">
+      <div className="w-full flex md:w-1/2 flex-col justify-center items-center p-8 md:p-12">
+        <form
+        onSubmit={handleSubmit}
+         className="w-full max-w-md bg-white p-8 rounded-lg border border-gray-500 shadow-sm">
+          <div className="flex justify-center mb-6">
+            <h2 className="text-xl font-medium">Rabbit</h2>
+          </div>
+          <h2 className="text-2xl font-bold text-center mb-6">Hey there! 👋</h2>
+          <p className="text-center mb-6 ">
+            Enter all neccessarry info to create new account.
+          </p>
+          <div className="mb-4">
+            <label className="block text-sm font-semibold mb-2">Fullname</label>
+            <input
+              type="fullname"
+              value={fullname}
+              onChange={(e) => setFullname(e.target.value)}
+              placeholder="Enter your Fullname"
+              className="w-full p-2 border border-gray-500   rounded invalid:border-pink-500 invalid:text-pink-600 focus:border-sky-500 focus:outline focus:outline-sky-500 focus:invalid:border-pink-500 focus:invalid:outline-pink-500 "
+              autoFocus
+            />
+          </div>
+
+          <div className="mb-4">
+            <label className="block text-sm font-semibold mb-2">Email</label>
+            <input
+              type="email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              placeholder="Enter your email Address"
+              className="w-full p-2 border border-gray-500  rounded invalid:border-pink-500 invalid:text-pink-600 focus:border-sky-500 focus:outline focus:outline-sky-500 focus:invalid:border-pink-500 focus:invalid:outline-pink-500 "
+            />
+          </div>
+
+          <div className="mb-4">
+            <label className="block text-sm font-semibold mb-2">Password</label>
+            <input
+              type="password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              placeholder="Enter your password"
+              className="w-full p-2 border border-gray-500  rounded invalid:border-pink-500 invalid:text-pink-600 focus:border-sky-500 focus:outline focus:outline-sky-500 focus:invalid:border-pink-500 focus:invalid:outline-pink-500"
+            />
+          </div>
+          <button
+          type="submit"
+           className="w-full bg-black text-white p-2 rounded-lg font-semibold hover:bg-gray-800 transition capitalize">
+            sign In
+          </button>
+          <p className="mt-6 text-center text-sm">
+            Already you have an account?{" "}
+            <Link to="/login" className="text-blue-500 hover:underline">
+              Log in
+            </Link>
+          </p>
+        </form>
+      </div>
+
+      <div className="hidden md:block w-1/2 bg-gray-800">
+        <div className="w-full flex flex-col justify-center items-center">
+          <img
+            src={register}
+            alt="Sign up image"
+            className="h-[750px] w-full object-cover"
+          />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default SignUp;
