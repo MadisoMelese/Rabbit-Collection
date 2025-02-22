ad6277971cb19f97655c908b47f15df823eda864
Madiso Melese <madishamadiso00@gmail.com>
2025-02-22 23:59:47 +0300

    MongoDB connected!

--- backend/index.js
+++ backend/index.js
@@ -1,1 +1,20 @@
-console.log("Server side started")
+import express from 'express'
+import cors from 'cors'
+import dotenv from 'dotenv'
+import connectDB from './config/db.js'
+
+const app = express()
+app.use(express.json())
+app.use(cors())
+dotenv.config()
+
+const PORT = process.env.PORT || 3000
+
+app.get("/", (req, res)=>{
+  res.send("welcome madisha API!")
+})
+
+app.listen(PORT, ()=>{
+  connectDB()
+  console.log(`Server is running on http://localhost:${PORT}`)
+})
