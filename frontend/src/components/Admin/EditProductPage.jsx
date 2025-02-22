76780913af81d81417483c24085687adbc5f71ec
Madiso Melese <madishamadiso00@gmail.com>
2025-02-22 18:25:11 +0300

    edit product image upload page error handled

--- frontend/src/components/Admin/EditProductPage.jsx
+++ frontend/src/components/Admin/EditProductPage.jsx
@@ -18,15 +18,15 @@ const EditProductPage = () => {
     images: [
       {
         url: "https://picsum.photos/500/500?random=1",
-        altText: "Stylish Jackect",
+        altText: "Stylish Jacket",
       },
       {
         url: "https://picsum.photos/500/500?random=2",
-        altText: "Stylish Jackect",
+        altText: "Stylish Jacket",
       },
       {
         url: "https://picsum.photos/500/500?random=3",
-        altText: "Stylish Jackect",
+        altText: "Stylish Jacket",
       },
     ],
   });
@@ -39,17 +39,34 @@ const EditProductPage = () => {
     }));
   };
 
-  const handleImageUpload = async (e)=>{
-    const file = e.taget.files[0]
-    console.log(file)
+  const handleImageUpload = async (e) => {
+    const file = e.target.files[0];
+    console.log(file);
+    // You can handle the image upload logic here
+  };
+
+  const handleSubmit = (e)=>{
+    e.preventDefault();
+    console.log({
+      name: productData.name,
+      description: productData.description,
+      price: productData.price,
+      countInStock: productData.countInStock,
+      sku: productData.sku,
+      sizes: productData.sizes,
+      colors: productData.colors,
+      image: productData.images,
+
+    })
   }
+
   return (
     <div className="max-w-5xl mx-auto p-6 shadow-md rounded-md">
       <h2 className="text-3xl font-bold mb-6">Edit Product</h2>
 
       {/* form */}
-      <form>
-        {/* name */}
+      <form onSubmit={handleSubmit}>
+        {/* Product Name */}
         <FormInput
           label="Product Name"
           name="name"
@@ -63,17 +80,18 @@ const EditProductPage = () => {
           <label htmlFor="description" className="block text-gray-700">
             Description
           </label>
-          <textArea
+          <textarea
             name="description"
             value={productData.description}
             rows={4}
             required
             className="w-full p-2 border rounded"
-          ></textArea>
+            onChange={handleChange}
+          />
         </div>
 
-           {/* Price range */}
-           <FormInput
+        {/* Price */}
+        <FormInput
           label="Price"
           name="price"
           value={productData.price}
@@ -81,56 +99,79 @@ const EditProductPage = () => {
           type="number"
         />
 
-           {/*count is stock */}
-           <FormInput
-          label="count in Stock"
+        {/* Count in Stock */}
+        <FormInput
+          label="Count in Stock"
           name="countInStock"
           value={productData.countInStock}
           onChange={handleChange}
           type="number"
         />
-
-           {/*SKU */}
-           <FormInput
-          label="sku"
+        {/* SKU */}
+        <FormInput
+          label="SKU"
           name="sku"
           value={productData.sku}
           onChange={handleChange}
           type="text"
         />
 
-           {/*sizes */}
-           <FormInput
-          label="Sizes (separeted-by-comma)"
+        {/* Sizes */}
+        <FormInput
+          type="text"
+          label="Sizes (separated-by-comma)"
           name="sizes"
           value={productData.sizes.join(", ")}
-          onChange={(e)=>setProductData({
-            ...productData,
-            sizes: e.target.value.split(",").map((size)=>size.trim()),
-          })}
-          type="text"
+          onChange={(e) =>
+            setProductData({
+              ...productData,
+              sizes: e.target.value.split(",").map((size) => size.trim()),
+            })
+          }
         />
 
-           {/*Colors */}
-           <FormInput
-          label="Colors - (separeted-by-comma)"
+        {/* Colors */}
+        <FormInput
+          type="text"
+          label="Colors (separated-by-comma)"
           name="colors"
           value={productData.colors.join(", ")}
-          onChange={(e)=>setProductData({
-            ...productData,
-            colors: e.target.value.split(",").map((color)=>color.trim()),
-          })}
-          type="text"
+          onChange={(e) =>
+            setProductData({
+              ...productData,
+              colors: e.target.value.split(",").map((color) => color.trim()),
+            })
+          }
         />
 
-      {/*Image upload  */}
-      <FormInput
-          label="Upload Image"
-          name="images"
-          value={productData.images.url}
-          onChange={handleImageUpload}
-          type="file"
-        />
+        {/* Image upload */}
+        <div className="mb-4">
+          <label className="block text-gray-700">Upload Image</label>
+          <input
+            type="file"
+            name="images"
+            onChange={handleImageUpload}
+            className="w-full p-2 border rounded"
+          />
+          <div className="flex gap-4 mt-4">
+            {productData.images.map((image, index) => (
+              <div key={index}>
+                <img
+                  src={image.url}
+                  alt={image.altText}
+                  className="size-20 object-cover rounded-md shadow-md"
+                />
+              </div>
+            ))}
+          </div>
+        </div>
+
+        {/* Submit Button */}
+        <button 
+        type="submit"
+        className="w-full bg-green-500 text-white py-2 rounded-md hover:bg-green-600 transition-colors">
+          Submit
+        </button>
       </form>
     </div>
   );
