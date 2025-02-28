c03185d6d84e484b5f7cc93db2c362fbc72b3ed9
Madiso Melese <madishamadiso00@gmail.com>
2025-02-28 23:50:53 +0300

    product schema added

---
+++ backend/models/product.js
@@ -0,0 +1,41 @@
+import mongoose from "mongoose"
+
+const productSchema = new mongoose.Schema({
+  name:{
+    type:String,
+    required:true,
+    trim:true,
+  },
+  description:{
+    type:String,
+    required:true,
+  },
+  price:{
+    type:Number,
+    required:true,
+  },
+  discountPrice:{
+    type:Number,
+  },
+  countInStock:{
+    type:Number,
+    required:true,
+    default:0
+  },
+  sku:{
+    type:String,
+    unique:true,
+    required:true
+  },
+  category:{
+    type:String,
+    required:true
+  },
+  brand:{
+    type:String,
+  },
+  sizes:{
+    type:[String],
+    required:true
+  },
+})
