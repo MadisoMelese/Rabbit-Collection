68446f0c93f29f0df6a105e0ea10569805d1b982
Madiso Melese <madishamadiso00@gmail.com>
2025-03-13 23:57:35 +0300

    chechout backend logic added

--- backend/controllers/checkout.controller.js
+++ backend/controllers/checkout.controller.js
@@ -1,11 +1,106 @@
 import { Checkout } from "../models/Checkout.js";
-import { Cart } from './../models/Cart.js';
-import Product from './../models/product.js'
+import { Cart } from "./../models/Cart.js";
+import Product from "./../models/product.js";
 import { Order } from "../models/Order.js";
 
-
+// @create new check out session
+// route: private
 const createCheckout = async (req, res) => {
-  
-}
+  const { checkoutItems, shippingAddress, paymentMethod, totalPrice } =
+    req.body;
+  if (!checkoutItems || checkoutItems.length === 0) {
+    return res
+      .status(400)
+      .json({ success: false, messsage: "no items found in check out!" });
+  }
+  try {
+    const createCheckout = await Checkout.create({
+      user: req.user._id,
+      checkoutItems: checkoutItems,
+      shippingAddress,
+      paymentMethod,
+      totalPrice,
+      paymentStatus: "Pending",
+      isPaid: false,
+    });
+    console.log(`Checkout created for user: ${req.user._id}`);
+    res.status(201).json({ success: true, createCheckout });
+  } catch (error) {
+    console.error("Server error in creating check out", error);
+    res
+      .status(500)
+      .json({ success: false, message: "Server error in creating check out" });
+  }
+};
 
-export {createCheckout, }
+// update checkout to mark as paid after successful payment
+// route: private
+const updateCheckout = async (req, res) => {
+  const { paymentStatus, paymentDetails } = req.body;
+
+  try {
+    const checkout = await Checkout.findOne(req.params.id);
+    if (!checkout) {
+      return res
+        .status(404)
+        .json({ success: false, message: "check out not fonund!" });
+    }
+    if (paymentStatus === "paid") {
+      checkout.isPaid = true;
+      checkout.paymentStatus = paymentStatus;
+      checkout.paymentDetails = paymentDetails;
+      checkout.paidAt = Date.now();
+      await checkout.save();
+      return res.status(200).json(checkout)
+    }else{
+      return res.status(400).json({success:false, message:"Invalid payment status"})
+    }
+  } catch (error) {
+    console.error("Server Error in update chack out", error);
+    res
+      .status(500)
+      .json({ success: false, message: "Server Error in update chack out" });
+  }
+};
+
+// finalize order
+// finalize checkout 
+const finalizeCheckout = async (req, res) => {
+  try {
+    const checkout = await Checkout.findById(req.params.id)
+    if (!checkout) {
+      return res.status(404).json({success:false, message: "Chech out not foound"})
+    }
+    if (checkout.isPaid && !checkout.isFinalized) {
+      // create final order based on the checkout details
+      const finalOrder = await Order.create({
+        user:user.checkout, 
+        orderItems:checkout.orderItems,
+        shippingAddress:ch  .shippingAddress,
+        paymentMethod:checkout.paymentMethod,
+        paymentDetails:checkout.paymentDetails,
+        totalPrice:checkout.totalPrice,
+        isPaid:true,
+        paidAt: checkout.paidAt,
+        isDelivered:false,
+        paymentStatus:"paid",
+      })
+
+      // mark the checkout as finalized
+      checkout.isFinalized=true;
+    checkout.finalizedAt= Date.now();
+    await checkout.save()
+    // Delete the cart associated with user
+    await Cart.findOneAndDelete({user:checkout.user})
+    return res.status(201).json({success:true, finalOrder})
+      } else if(checkout.isFinalized){
+      return res.status(400).json({success:false, message:"chechout already finalized!"})
+    }else{
+      return res.status(400).json({success:false, message:"checkout is not paid!"})
+    }
+  } catch (error) {
+    console.error("Server error in finalize check out!", error)
+    res.status(500).json({success:false, message:"Server error in finalize check out!"})
+  }
+}
+export { createCheckout, updateCheckout, finalizeCheckout };
--- backend/routes/checkoutRoutes.js
+++ backend/routes/checkoutRoutes.js
@@ -1,8 +1,10 @@
 import express from 'express';
 import { protectAuth } from './../middleWare/protectAuth.js';
-import {createCheckout, } from '../controllers/checkout.controller.js'
+import {createCheckout, updateCheckout, finalizeCheckout} from '../controllers/checkout.controller.js'
 const router = express.Router()
 
 router.post('/', protectAuth, createCheckout);
+router.put('/:id/pay', protectAuth, updateCheckout);
+router.put('/:id/finalized', protectAuth, updateCheckout);
 
 export default router
