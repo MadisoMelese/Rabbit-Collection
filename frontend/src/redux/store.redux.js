edda82e9c11a133f6690eabda9f59b913b0fe557
Madiso Melese <madishamadiso00@gmail.com>
2025-03-26 15:05:39 +0300

    Redux store updated

--- frontend/src/redux/store.redux.js
+++ frontend/src/redux/store.redux.js
@@ -5,6 +5,7 @@ import cartReducer from './slice/cart.Slice.js';
 import checkoutReducer from './slice/checkout.Slice.js';
 import orderReducer from './slice/order.Slice.js';
 import adminReducer from './slice/admin.Slice.js';
+import adminProductReducer from './slice/admin.product.Slice.js'; 
 
 const store = configureStore({
   reducer: {
@@ -14,6 +15,7 @@ const store = configureStore({
   checkout: checkoutReducer,
   orders:orderReducer,
   admin: adminReducer,
+  adminProduct: adminProductReducer,
   },
 });
 
