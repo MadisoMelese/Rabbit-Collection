3b750530b65c4fee81d2cfcd5356092ebdb95c7c
Madiso Melese <madishamadiso00@gmail.com>
2025-04-01 02:18:07 +0300

    sort option error handled

--- frontend/src/components/Products/SortOption.jsx
+++ frontend/src/components/Products/SortOption.jsx
@@ -1,20 +1,17 @@
 import { useSearchParams } from "react-router-dom";
 
 const SortOption = () => {
-  const [, setSearchParams] = useSearchParams()
+  const [searchParams, setSearchParams] = useSearchParams()
   const sortChangeHandler = (e) => {
     const sortBy = e.target.value;
-    if (sortBy === "") {
-      setSearchParams({});
-    } else {
-      setSearchParams({ sort: sortBy });
-    }
-
+    searchParams.set("sortBy", sortBy)
+    setSearchParams(searchParams)
   }
   return (
     <div className="mb-4 flex items-center justify-end">
       <select 
       onChange={sortChangeHandler}
+      value={searchParams.get("sortBy") || ""}
       id="sort" className="border p-2 rounded-md focus:outline-none">
         <option value="">Default</option>
         <option value="priceAsc">Price: Low to High</option>
